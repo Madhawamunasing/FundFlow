a337d6e0341832175ffdd55e2371f52a704bf646
Madhawa Munasinghe <63332810+Madhawamunasing@users.noreply.github.com>
2025-05-11 18:58:00 +0530

    added docker files

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
-const customerRoutes = require('./routes/customerRoutes');
-const loanRoutes = require('./routes/loanRoutes');
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const authRoutes = require('./routes/authRoutes')
+const customerRoutes = require('./routes/customerRoutes')
+const loanRoutes = require('./routes/loanRoutes')
 
-const swaggerUI = require('swagger-ui-express');
-const swaggerSpec = require('./docs/swagger');
+const swaggerUI = require('swagger-ui-express')
+const swaggerSpec = require('./docs/swagger')
 
-const {connectMySQL} = require('./config/db');
-const connectMongo = require('./config/mongo');
+const { connectMySQL } = require('./config/db')
+const connectMongo = require('./config/mongo')
 
-const app = express();
-dotenv.config();
+const app = express()
+dotenv.config()
 
-connectMySQL();
-connectMongo();
+connectMySQL()
+connectMongo()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use('/api/auth', authRoutes);
-app.use('/api/customers', customerRoutes);
-app.use('/api/loans', loanRoutes);
+app.use('/api/auth', authRoutes)
+app.use('/api/customers', customerRoutes)
+app.use('/api/loans', loanRoutes)
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
-module.exports = app;
+module.exports = app
